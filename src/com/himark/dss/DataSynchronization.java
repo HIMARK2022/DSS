24e28442f510839df7ca293457b6b6243ef6bac2
SuperRookiee <hyunuk0417@gmail.com>
2022-03-29 15:37:41 +0900

    마크애니 DB의 IP주소, 스키마, ID, PW 수정

--- src/com/himark/dss/DataSynchronization.java
+++ src/com/himark/dss/DataSynchronization.java
@@ -5,19 +5,19 @@ import java.sql.Connection;
 public class DataSynchronization {
 	public static void main(String[] args) {
 		// 마크애니 DB
-		String m_url = "jdbc:mysql://192.168.1.11:3306/markanydb?useSSL=false";
-		String m_dbId = "markany";
-		String m_dbPwd = "markany";
+		String m_url = "jdbc:mysql://192.168.1.57:3306/madb?useSSL=false";
+		String m_dbId = "ma";
+		String m_dbPwd = "ma";
 		
 		// 고객사 DB (개발 완료 후 jar 실행 시 입력받을 수 있도록 처리)
-//		String c_url = "jdbc:mysql://" + args[0] + ":3306/" + args[1] + "?useSSL=false";
-//		String c_dbId = args[2];
-//		String c_dbPwd = args[3];
+		String c_url = "jdbc:mysql://" + args[0] + ":3306/" + args[1] + "?useSSL=false";
+		String c_dbId = args[2];
+		String c_dbPwd = args[3];
 		
 		// 고객사 DB
-		String c_url = "jdbc:mysql://192.168.1.11:3306/clientdb?useSSL=false";
-		String c_dbId = "client";
-		String c_dbPwd = "client";
+//		String c_url = "jdbc:mysql://192.168.1.11:3306/clientdb?useSSL=false";
+//		String c_dbId = "client";
+//		String c_dbPwd = "client";
 		
 		// DB 연결
 		Connection m_conn = DBConnection.getConnectivity(m_url, m_dbId, m_dbPwd);
@@ -35,7 +35,6 @@ public class DataSynchronization {
 		DataProcessing.manageTable(m_conn, c_conn, "dept");
 		DataProcessing.manageTable(m_conn, c_conn, "duty");
 		DataProcessing.manageTable(m_conn, c_conn, "pos");
-		DataProcessing.manageTable(m_conn, c_conn, "manager");
 	}
 		
 }
