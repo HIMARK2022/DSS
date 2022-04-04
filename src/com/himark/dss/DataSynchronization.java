7009c9ffad1aac095ad336ac54c3de8fef672e40
SSoohyun <chltngus129@gmail.com>
2022-04-04 14:31:46 +0900

    프로퍼티 파일 읽어오기 프로퍼티 파일을 읽고, 그 내용을 확인하였다.

--- src/com/himark/dss/DataSynchronization.java
+++ src/com/himark/dss/DataSynchronization.java
@@ -1,53 +1,63 @@
 package com.himark.dss;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import com.himark.data.Dept;
-import com.himark.service.DeptService;
+import com.himark.data.Duty;
+import com.himark.data.Pos;
+import com.himark.data.User;
 
 public class DataSynchronization {
 	public static void main(String[] args) {
-		// 마크애니 DB
-//		String m_url = "jdbc:mysql://192.168.1.57:3306/madb?useSSL=false";
-//		String m_dbId = "ma";
-//		String m_dbPwd = "ma";
 
-		// 고객사 DB (개발 완료 후 jar 실행 시 입력받을 수 있도록 처리)
-//		String c_url = "jdbc:mysql://" + args[0] + ":3306/" + args[1] + "?useSSL=false";
-//		String c_dbId = args[2];
-//		String c_dbPwd = args[3];
-
-		// 고객사 DB
-//		String c_url = "jdbc:mysql://192.168.1.11:3306/clientdb?useSSL=false";
-//		String c_dbId = "client";
-//		String c_dbPwd = "client";
-
-		// DB 연결
-//		Connection m_conn = DBConnection.getConnectivity(m_url, m_dbId, m_dbPwd);
-//		Connection c_conn = DBConnection.getConnectivity(c_url, c_dbId, c_dbPwd);
-
-		// DB 연결 테스트
-//		System.out.println("마크애니 DB 연결 테스트");
-//		DBConnection.connectionTest(m_conn);
-
-//		System.out.println("고객사 DB 테스트");
-//		DBConnection.connectionTest(c_conn);
-
-		// 인사연동
-//		DataProcessing.manageTable(m_conn, c_conn, "user");
-//		DataProcessing.manageTable(m_conn, c_conn, "dept");
-//		DataProcessing.manageTable(m_conn, c_conn, "duty");
-//		DataProcessing.manageTable(m_conn, c_conn, "pos");
-		
-		
 		// dept 테이블로 MyBatis 테스트
-		DeptService deptService = new DeptService();
-		List<Dept> deptList = deptService.select();
+//		DeptService deptService = new DeptService();
+//		List<Dept> deptList = deptService.select();
+//		
+//		for(Dept dept : deptList) {
+//			System.out.println(dept.getDeptId() + " " + dept.getDeptName() + " " + dept.getUpperDeptId());
+//		}
 		
-		for(Dept dept : deptList) {
-			System.out.println(dept.getDeptId() + " " + dept.getDeptName() + " " + dept.getUpperDeptId());
+		// 프로퍼티 파일을 읽어서 저장할 리스트
+		List<String> properties = new ArrayList<String>();
+		
+		// 프로퍼티 파일 읽어오기
+		try {
+			// 파일 객체 생성
+			File file = new File("C:/properties/dream.properties"); // 파일은 입력받을 수 있도록 수정할 예정
+			
+			// 입력 스트림 생성
+            FileReader filereader = new FileReader(file);
+            
+            // 입력 버퍼 생성
+            BufferedReader bufReader = new BufferedReader(filereader);
+            
+            String line = "";
+            line = bufReader.readLine(); // .readLine()은 끝에 개행문자를 읽지 않음            
+            bufReader.close();
+            
+            properties = Arrays.asList(line.split(","));
+            
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 		
+		// 리스트 확인
+		for(String s : properties) {
+			System.out.println(s);
+		}
+				
 	}
 
 }
